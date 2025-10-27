33a76b1c0214d1c8ce69853472da20b3f146e0bc
EmilyMacias <99148529+EmilyMacias@users.noreply.github.com>
2025-10-26 17:14:56 -0700

    small fix

--- logic.js
+++ logic.js
@@ -93,7 +93,7 @@ d3.csv("data/SF_Film_Locations_Filtered.csv").then((data) => {
   // apply year slider filter
   yearSlider.addEventListener("input", () => {
     const relevantData = data.filter((d) =>
-      console.log(+d["ReleaseYear"], +yearSlider.value)
+      console.log(+d["Release Year"], +yearSlider.value)
     );
 
     console.log(relevantData);
