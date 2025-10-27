48372ea5f0b263e19f1bdd3baa37c9b5532e4752
EmilyMacias <99148529+EmilyMacias@users.noreply.github.com>
2025-10-26 17:33:44 -0700

    color

--- logic.js
+++ logic.js
@@ -74,7 +74,7 @@ d3.csv("data/SF_Film_Locations_Filtered.csv").then((data) => {
     .attr("cx", projection([-122.45, 37.75])[0])
     .attr("cy", projection([-122.45, 37.75])[1])
     .attr("r", 100)
-    .attr("fill", "rgba(0,0,255,0.2)")
+    .attr("fill", "rgba(0,0,255,0.15)")
     .attr("stroke", "blue")
     .attr("stroke-width", 2)
     .raise();
@@ -85,7 +85,7 @@ d3.csv("data/SF_Film_Locations_Filtered.csv").then((data) => {
     .attr("cx", projection([-122.4, 37.75])[0])
     .attr("cy", projection([-122.4, 37.75])[1])
     .attr("r", 100)
-    .attr("fill", "rgba(255,0,0,0.2)")
+    .attr("fill", "rgba(255,0,0,0.15)")
     .attr("stroke", "red")
     .attr("stroke-width", 2)
     .raise();
